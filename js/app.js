06efa1bf8c594e0fb312cb48821ab41b212ac694
Wellington Gaino <wellingtongssantos@gmail.com>
2025-11-08 22:53:09 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,44 @@
+let listaDeAlugados = [];
+
+
+
+
+
+
+function alterarStatus(idJogo){
+    let containerJogo = document.getElementById(`game-${idJogo}`)
+    let imagemJogo = containerJogo.querySelector('.dashboard__item__img')
+    let botao = containerJogo.querySelector('.dashboard__item__button')
+    let nomeDoJogo = containerJogo.querySelector('.dashboard__item__name').textContent;
+
+    console.log(nomeDoJogo);
+
+
+    let imagemApagada = ('dashboard__item__img--rented')
+    let botaoApagado = ('dashboard__item__button--return')
+
+
+    if(imagemJogo.classList.contains(imagemApagada)){
+        imagemJogo.classList.remove(imagemApagada)
+        botao.classList.remove(botaoApagado)
+        botao.textContent = 'Alugar'
+        alert ('Item devolvido, obrigado!');
+        listaDeAlugados.pop(nomeDoJogo)
+        console.log (listaDeAlugados);
+
+
+
+    } else {
+        imagemJogo.classList.add(imagemApagada)
+        botao.classList.add(botaoApagado)
+        botao.textContent = 'Devolver'
+        alert ('Item alugado, obrigado!')
+        listaDeAlugados.push(nomeDoJogo)
+        console.log(listaDeAlugados);
+    
+    
+
+    }
+
+
+}
